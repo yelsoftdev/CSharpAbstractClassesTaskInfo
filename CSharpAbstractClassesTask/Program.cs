9b2e50cb983fe0ce7167185ead197f97cb8728d8
ygurusamy <ygurusamy@YGURUSAMY-LAP>
2022-11-23 16:16:35 +0530

    Add project files.

---
+++ CSharpAbstractClassesTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharpAbstractClassesTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Console.WriteLine("*******Input*******");
+            Console.Write("Enter the dog name: ");
+            dog.SetName(Console.ReadLine());
+            Console.WriteLine("\n*******Output*******");
+            Console.WriteLine(String.Format($"Name: {dog.GetName()}"));
+            dog.Eat();
+        }
+    }
+    public abstract class Animal
+    {
+        private string Name;
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public abstract void Eat();
+    }
+    public class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Dog is Eating");
+        }
+    }
+}
